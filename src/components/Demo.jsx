02c7f82999ed443bf55597bc119443b5f5039195
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-29 22:58:48 +0700

    update-AddBrowserHistory

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
 
@@ -8,9 +9,51 @@ const Demo = () => {
         summary: "",
     });
 
+    const [allArticles, setAllArticles] = useState([]);
+    const [copied, setCopied] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+    useEffect(() => {
+        const articleFromLocalStorage = JSON.parse(
+            window.localStorage.getItem("articles")
+        );
+
+        if(articleFromLocalStorage) {
+            setArticle(articleFromLocalStorage);
+        }
+    }, []);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log('Submitted!');
+
+        const existingArticle = allArticles.find(
+            (item) => item.url === article.url,
+        );
+
+        if(existingArticle) return setArticle(existingArticle);
+
+        const { data } = await getSummary({
+            articleUrl: article.url,
+        });
+
+        if(data?.summary) {
+            const newArticle = { ...article, summary: data.summary };
+            const updatedAllArticles = [newArticle, ...allArticles];
+
+             // update state and local storage
+            setArticle(newArticle);
+            setAllArticles(updatedAllArticles);
+            localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+        }
+        console.log(data);
+    };
+
+    const handleCopy = (copyUrl) => {
+        setCopied(copyUrl);
+        navigator.clipboard.writeText(copyUrl);
+        setTimeout(() => setCopied(false), 3000);
     };
 
     const handleKeyDown = (e) => {
@@ -40,7 +83,7 @@ const Demo = () => {
                         <input
                             type='url'
                             placeholder='Paste the article link'
-                            value={article.url}
+                            value={article.url || ''}
                             onChange={(e) => setArticle({...article, url: e.target.value})}
                             onKeyDown={handleKeyDown}
                             required
@@ -51,10 +94,50 @@ const Demo = () => {
                             className='submit_btn
                             peer-focus:border-gray-700
                             peer-focus:text-gray-700'
+                            disabled={isLoading}
                         >
-                            <p>↵</p>
+                            {isLoading
+                                ?<img
+                                    src={loader}
+                                    alt="loading icon"
+                                    className="w-4 h-4"
+                                />
+                                : <p>↵</p>
+                            }
                         </button>
                     </form>
+
+                    {/* Browser History */}
+                    <div
+                        className='flex flex-col gap-1
+                        max-h-60 overflow-y-auto'
+                    >
+                        {allArticles.map((item, index) => (
+                            <div
+                                key={`link-${index}`}
+                                tabIndex={index}
+                                onClick={() => setArticle(item)}
+                                className='link_card'
+                            >
+                                <div
+                                    className='copy-btn'
+                                    onClick={() => handleCopy(item.url)}
+                                >
+                                    <img
+                                        src={copied === item.url ? tick : copy}
+                                        alt={copied === item.url ? "tick_icon" : "copy_icon"}
+                                        className='w-[100%] h-[100%] object-contain'
+                                    />
+                                </div>
+                                <p
+                                    className='flex-1 text-blue-500
+                                    font-medium text-sm truncate'
+                                >
+                                    {item.url}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </section>
         </>
